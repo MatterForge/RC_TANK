851b9d88a1645e757e004869bb2be37a8c95e539
MatterForge <thematterforge@gmail.com>
2022-12-04 13:24:31 -0500

    Added arming sequence and led functions

--- RC_TANK/RC_TANK.ino
+++ RC_TANK/RC_TANK.ino
@@ -31,12 +31,29 @@ int ch1Map;
 int ch1MapSmooth;
 int ch1MapPrev;
 #define ch5 7
+int ch5Val;
 #define ch6 6
+int ch6Val;
+
+int right[3] = {38,40,42};
+int left[3] = {32,34,36};
+int backLeft = 30;
+int backRight = 28;
+
+// pinMode(left[0], OUTPUT);
+// pinMode(left[1], OUTPUT);
+// pinMode(left[2], OUTPUT);
+// pinMode(right[0], OUTPUT);
+// pinMode(right[1], OUTPUT);
+// pinMode(right[2], OUTPUT);
+// pinMode(backLeft, OUTPUT);
+// pinMode(backRight, OUTPUT);
+
 
 void setup() {
   Serial.begin(115200);
-  pinMode(ch2,INPUT);
   pinMode(ch1,INPUT);
+  pinMode(ch2,INPUT);
   pinMode(ch5,INPUT);
   pinMode(ch6,INPUT);
   serv1.detach();
@@ -48,7 +65,20 @@ void setup() {
 void loop() {
   getPulses();
   getVoltage();
-  runMaps();
+  Serial.print("Volts: ");Serial.println(volts); 
+  if(ch5Val > 1700) {
+    runMaps();
+    Serial.print("Ch2:");Serial.print(ch2MapSmooth);
+    Serial.print("Ch2 Inv:");Serial.println(ch2MapInvSmooth);
+    Serial.print("Ch1:");Serial.print(ch1Val);
+    Serial.print("Ch5:");Serial.print(ch5Val);
+    Serial.print("Ch6:");Serial.println(ch6Val);
+  }
+  else{
+    //lights blink
+  }
+  // Serial.print("Ch2:");Serial.print(ch2MapSmooth);
+  // Serial.print("Ch2 Inv:");Serial.print(ch2MapInvSmooth);
 }
 
 void runMaps() {
@@ -74,8 +104,6 @@ void runMaps() {
   ch2MapSmooth = constrain(ch2MapSmooth,70,110);
   ch2MapInvSmooth = constrain(ch2MapInvSmooth,70,110);
 
-  Serial.print("Ch2:");Serial.print(ch2MapSmooth);
-  Serial.print("Ch2:");Serial.print(ch2MapInvSmooth);
 
   if (ch2MapSmooth < 87 || ch2MapSmooth > 90 || ch1Map < -5 || ch1Map > 5) {
     serv1.attach(10);
@@ -102,10 +130,73 @@ void getPulses() {
   ch2Val = constrain(ch2Val,985,1995);
   ch1Val = pulseIn(ch1, HIGH);
   ch1Val = constrain(ch1Val,1040,1940);
+  ch5Val = pulseIn(ch5, HIGH);
+  ch5Val = constrain(ch5Val,1040,1940);
+  ch6Val = pulseIn(ch6, HIGH);
+  ch6Val = constrain(ch6Val,1040,1940);
 }
 
 void getVoltage() {
   cellVal = analogRead(cellPin);
-  volts = ((cellVal / 1023.0) * refVolts);
-  Serial.print("   |   Volts: ");Serial.println(volts);  
+  volts = ((cellVal / 1023.0) * refVolts); 
 }
+
+void leftOn() {
+  digitalWrite(left[0], HIGH);
+  digitalWrite(left[1], HIGH);
+  digitalWrite(left[2], HIGH);
+};
+void leftOff() {
+  digitalWrite(left[0], LOW);
+  digitalWrite(left[1], LOW);
+  digitalWrite(left[2], LOW);
+};
+void rightOn() {
+  digitalWrite(right[0], HIGH);
+  digitalWrite(right[1], HIGH);
+  digitalWrite(right[2], HIGH);
+};
+void rightOff() {
+  digitalWrite(right[0], LOW);
+  digitalWrite(right[1], LOW);
+  digitalWrite(right[2], LOW);
+};
+void backLeftOn() {
+  digitalWrite(backLeft, HIGH);
+};
+void backLeftOff() {
+  digitalWrite(backLeft, LOW);
+};
+void backRightOn() {
+  digitalWrite(backRight, HIGH);
+};
+void backRightOff() {
+  digitalWrite(backRight, LOW);
+};
+void lightsOn() {
+  digitalWrite(left[0], HIGH);
+  digitalWrite(left[1], HIGH);
+  digitalWrite(left[2], HIGH);
+  digitalWrite(right[0], HIGH);
+  digitalWrite(right[1], HIGH);
+  digitalWrite(right[2], HIGH);
+  digitalWrite(backLeft, HIGH);
+  digitalWrite(backRight, HIGH);
+};
+void lightsOff() {
+  digitalWrite(left[0], LOW);
+  digitalWrite(left[1], LOW);
+  digitalWrite(left[2], LOW);
+  digitalWrite(right[0], LOW);
+  digitalWrite(right[1], LOW);
+  digitalWrite(right[2], LOW);
+  digitalWrite(backLeft, LOW);
+  digitalWrite(backRight, LOW);
+};
+
+
+
+
+
+
+
